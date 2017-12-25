b5c43f1ab136fffd439077fc14ca02a97be0e930
Dmitriy Kuzmenko <dmitriy.kuzmenko@cxense.com>
2017-12-25 17:23:29 +0400

    Переход на MongoDB для ускорения разработки, подправил шаблоны

--- mylibrary/mylibrary.py
+++ mylibrary/mylibrary.py
@@ -1,51 +1,19 @@
 import os
-import sqlite3
+import settings
+import pymongo
 
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
-app = Flask(__name__) # create the application instance :)
-app.config.from_object(__name__) # load config from this file , flaskr.py
+app = Flask(__name__, template_folder='template') # create the application instance :)
 
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'mylibrary.db')
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
-app.config.from_envvar('MYLIBRARY_SETTINGS', silent=True)
+mng_conn = pymongo.MongoClient(host=settings.db['host'], port=settings.db['port'])
+db = mng_conn.client[settings.db['database']]
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
+@app.route('/')
+def index_path():
+        return render_template('home.html')
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
-def init_db():
-    db = get_db()
-    with app.open_resource('mylibrary.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
-
-
+@app.route('/books')
+def books_path():
+        return render_template('books.html')
